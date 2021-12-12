22dad7fe51fff448a24eedd728c94b2ec7a89492
jaydeluca <jaydeluca4@gmail.com>
2021-12-12 12:59:09 -0500

    2021-day06

--- src/main/kotlin/twentytwentyone/Day06.kt
+++ src/main/kotlin/twentytwentyone/Day06.kt
@@ -1,17 +1,55 @@
 package twentytwentyone
 
 import common.FileInput
+import java.math.BigInteger
 
 
 class Day06 {
 
+    data class Puzzle(val lanternFish: MutableMap<Int, BigInteger>) {
+        companion object Factory {
+            fun parse(input: List<String>): Puzzle {
+                val fishList: MutableMap<Int, BigInteger> = mutableMapOf()
 
-    fun problemOne(input: List<String>): Int {
-        return 0
+                for (i in 0..8) {
+                    fishList[i] = BigInteger.ZERO
+                }
+
+                input[0].split(",")
+                    .groupBy(String::toInt)
+                    .forEach {
+                        fishList[it.key] = BigInteger.valueOf(it.value.size.toLong())
+                    }
+                return Puzzle(lanternFish = fishList)
+            }
+        }
+
+        fun totalFish(): BigInteger = lanternFish.map { it.value }.fold(BigInteger.ZERO, BigInteger::add)
+
+        fun numDaysProceed(days: Int) {
+            for (i in 0 until days) {
+                val startingZeros = lanternFish[0]
+
+                for (x in 0 until 8) {
+                    lanternFish[x] = lanternFish[x + 1]!!
+                }
+                lanternFish[8] = startingZeros!!
+                lanternFish[6] = lanternFish[6]!!.plus(startingZeros)
+            }
+        }
     }
 
-    fun problemTwo(input: List<String>): Int {
-        return 0
+
+    fun problemOne(input: List<String>): BigInteger {
+        val puzzle = Puzzle.parse(input)
+        puzzle.numDaysProceed(80)
+        return puzzle.totalFish()
+    }
+
+    fun problemTwo(input: List<String>): BigInteger {
+        val puzzle = Puzzle.parse(input)
+        puzzle.numDaysProceed(256)
+        return puzzle.totalFish()
     }
 }
 
--- src/test/kotlin/twentytwentyone/Day06KtTest.kt
+++ src/test/kotlin/twentytwentyone/Day06KtTest.kt
@@ -2,22 +2,25 @@ package twentytwentyone
 
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
+import java.math.BigInteger
 
 class Day06KtTest {
 
-    private val exampleInput: List<String> = listOf()
+    private val exampleInput: List<String> = listOf(
+        "3,4,3,1,2"
+    )
 
     @Test
     fun testPartOneWithExampleInput() {
         val solver = Day06()
         val result = solver.problemOne(exampleInput)
-        assertEquals(4512, result)
+        assertEquals(BigInteger.valueOf(5934), result)
     }
 
     @Test
     fun testPartTwoWithExampleInput() {
         val solver = Day06()
         val result = solver.problemTwo(exampleInput)
-        assertEquals(1924, result)
+        assertEquals(BigInteger.valueOf(26984457539), result)
     }
 }
