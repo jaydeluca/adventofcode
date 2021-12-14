940070494661f38b5dcb2b73649fdeb5c21f3855
jaydeluca <jaydeluca4@gmail.com>
2021-12-13 20:26:15 -0500

    2021-day07

--- src/main/kotlin/twentytwentyone/Day07.kt
+++ src/main/kotlin/twentytwentyone/Day07.kt
@@ -2,16 +2,57 @@ package twentytwentyone
 
 import common.FileInput
 
-
 class Day07 {
+
+    data class CrabDeets(
+        val positions: Map<Int, List<Int>>,
+        val max: Int
+    ) {
+        companion object Factory {
+            fun parse(input: List<String>): CrabDeets {
+                val parsed = input[0]
+                    .split(",")
+                    .map(String::toInt)
+                    .sorted()
+                return CrabDeets(
+                    positions = parsed.groupBy { it },
+                    max = parsed.last()
+                )
+            }
+        }
+    }
+
+    fun calculateFuel(
+        position: Int, horizontalPositions: Map<Int, List<Int>>
+    ) = horizontalPositions.map {
+        kotlin.math.abs(it.key - position) * it.value.size
+    }.sum()
+
+    fun calculateFuelExpensive(
+        position: Int, horizontalPositions: Map<Int, List<Int>>
+    ) = horizontalPositions.map {
+        val delta = kotlin.math.abs(it.key - position)
+        val cost = if (delta > 0) {
+            (1..delta).reduce(Int::plus)
+        } else 0
+        cost * it.value.size
+    }.sum()
 
 
     fun problemOne(input: List<String>): Int {
-        return 0
+        val crabDeets = CrabDeets.parse(input)
+
+        return (1..crabDeets.max).map {
+            calculateFuel(it, crabDeets.positions)
+        }.minOrNull() ?: 0
     }
 
     fun problemTwo(input: List<String>): Int {
-        return 0
+        val crabDeets = CrabDeets.parse(input)
+
+        return (1..crabDeets.max).map {
+            calculateFuelExpensive(it, crabDeets.positions)
+        }.minOrNull() ?: 0
     }
 }
 
--- src/test/kotlin/twentytwentyone/Day07KtTest.kt
+++ src/test/kotlin/twentytwentyone/Day07KtTest.kt
@@ -5,7 +5,24 @@ import org.junit.jupiter.api.Test
 
 class Day07KtTest {
 
-    private val exampleInput: List<String> = listOf()
+    private val exampleInput: List<String> = listOf(
+        "16,1,2,0,4,2,7,1,2,14"
+    )
+
+    @Test
+    fun testFuelCalculation() {
+        val solver = Day07()
+        val crabDeets = Day07.CrabDeets.parse(exampleInput)
+
+        val test1 = solver.calculateFuel(1, crabDeets.positions)
+        assertEquals(41, test1)
+
+        val test2 = solver.calculateFuel(3, crabDeets.positions)
+        assertEquals(39, test2)
+
+        val test3 = solver.calculateFuel(10, crabDeets.positions)
+        assertEquals(71, test3)
+    }
 
     @Test
     fun testPartOneWithExampleInput() {
@@ -15,9 +32,18 @@ class Day07KtTest {
     }
 
     @Test
+    fun testExpensiveFuelCalculation() {
+        val solver = Day07()
+        val crabDeets = Day07.CrabDeets.parse(exampleInput)
+
+        val test1 = solver.calculateFuelExpensive(5, crabDeets.positions)
+        assertEquals(168, test1)
+    }
+
+    @Test
     fun testPartTwoWithExampleInput() {
         val solver = Day07()
         val result = solver.problemTwo(exampleInput)
-        assertEquals(17, result)
+        assertEquals(168, result)
     }
 }
