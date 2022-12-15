76c1135e2b9b90aaf651dc50c51282bcc4a390dd
jaydeluca <jaydeluca4@gmail.com>
2022-12-15 16:02:38 -0500

    day9 part one

--- src/test/kotlin/twentytwentytwo/Day9Test.kt
+++ src/test/kotlin/twentytwentytwo/Day9Test.kt
@@ -24,9 +24,98 @@ class Day9Test {
     }
 
     @Test
+    fun testIsTouchingMethod() {
+        val head = Day9.Position(x=4, y=1)
+        val tail = Day9.Position(x=3, y=0)
+        assertTrue(head.isTouching(tail))
+    }
+
+    @Test
+    fun testMoves() {
+        val solver = Day9()
+        solver.parseInput(listOf("R 4"))
+
+        solver.moves.forEach {
+            (0 until it.spaces).forEach { cur ->
+                solver.headLocation.processMove(Day9.Move(direction = it.direction, spaces = 1))
+                val tailMove =  solver.headLocation.calculateTailMove(solver.tailLocation)
+                solver.tailLocation.processMove(tailMove)
+            }
+        }
+        assertEquals(solver.headLocation.x, 4)
+        assertEquals(solver.headLocation.y, 0)
+        assertEquals(solver.tailLocation.x, 3)
+        assertEquals(solver.tailLocation.y, 0)
+
+
+        solver.parseInput(listOf("U 4"))
+
+        solver.moves.forEach {
+            (0 until it.spaces).forEach { cur ->
+                solver.headLocation.processMove(Day9.Move(direction = it.direction, spaces = 1))
+                val tailMove =  solver.headLocation.calculateTailMove(solver.tailLocation)
+                solver.tailLocation.processMove(tailMove)
+            }
+        }
+
+        assertEquals(solver.headLocation.x, 4)
+        assertEquals(solver.headLocation.y, 4)
+        assertEquals(solver.tailLocation.x, 4)
+        assertEquals(solver.tailLocation.y, 3)
+
+
+        solver.parseInput(listOf("L 3"))
+
+        solver.moves.forEach {
+            (0 until it.spaces).forEach { cur ->
+                solver.headLocation.processMove(Day9.Move(direction = it.direction, spaces = 1))
+                val tailMove =  solver.headLocation.calculateTailMove(solver.tailLocation)
+                solver.tailLocation.processMove(tailMove)
+            }
+        }
+
+        assertEquals( 1, solver.headLocation.x)
+        assertEquals(4, solver.headLocation.y)
+        assertEquals(2, solver.tailLocation.x)
+        assertEquals(4, solver.tailLocation.y)
+
+
+        solver.parseInput(listOf("D 1"))
+
+        solver.moves.forEach {
+            (0 until it.spaces).forEach { cur ->
+                solver.headLocation.processMove(Day9.Move(direction = it.direction, spaces = 1))
+                val tailMove =  solver.headLocation.calculateTailMove(solver.tailLocation)
+                solver.tailLocation.processMove(tailMove)
+            }
+        }
+
+        assertEquals( 1, solver.headLocation.x)
+        assertEquals(3, solver.headLocation.y)
+        assertEquals(2, solver.tailLocation.x)
+        assertEquals(4, solver.tailLocation.y)
+
+        solver.parseInput(listOf("R 4"))
+
+        solver.moves.forEach {
+            (0 until it.spaces).forEach { cur ->
+                solver.headLocation.processMove(Day9.Move(direction = it.direction, spaces = 1))
+                val tailMove =  solver.headLocation.calculateTailMove(solver.tailLocation)
+                solver.tailLocation.processMove(tailMove)
+            }
+        }
+
+        assertEquals( 5, solver.headLocation.x)
+        assertEquals(3, solver.headLocation.y)
+        assertEquals(4, solver.tailLocation.x)
+        assertEquals(3, solver.tailLocation.y)
+
+    }
+
+    @Test
     fun testPartTwoExampleInput() {
         val solver = Day9()
         val result = solver.problemTwo(exampleInput)
-        assertEquals(5, result)
+        assertEquals(36, result)
     }
 }
